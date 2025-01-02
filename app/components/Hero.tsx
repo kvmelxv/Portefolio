7762055132f8c83b7170f3d638ab09334399b214
Adam <abelgadid@gmail.com>
2025-01-02 02:25:27 -0500

    edit font size

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -10,8 +10,8 @@ export const Hero = () => {
                 <div className="flex-[3]">
                     <h2 className="font-caption text-6xl mb-3 font-bold" >Hi,</h2>
                     <h2 className="font-caption text-7xl mb-3 font-bold" >I&apos;m Mustapha</h2>
-                    <h2 className="font-caption text-5xl mb-3 font-bold" >Kamel Cherif 👋</h2>
-                    <h3 className="text-2xl mb-3">Full Stack Developer</h3>
+                    <h2 className="font-caption text-6xl mb-3 font-bold" >Kamel Cherif 👋</h2>
+                    <h3 className="text-[23px] mb-3">Full Stack Developer</h3>
                     <p className="mb-2 text-md mb-3">I&apos;m a full-stack developer passionate about crafting scalable web applications that blend user-centric design with efficient functionality.</p>
                     <p className="text-md">Based in Montreal, Canada 🇨🇦</p>
                 </div>
